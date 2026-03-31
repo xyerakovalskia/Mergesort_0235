f58081d5558dea9ab72b36798878ac6e442c4d30
xyerakovalskia <xyera123987@gmail.com>
2026-03-31 10:50:03 +0700

    mergesort_2

--- mergesort.cpp
+++ mergesort.cpp
@@ -5,3 +5,16 @@ using namespace std;
 int arr[20], B[20];
 // n is array input size
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
