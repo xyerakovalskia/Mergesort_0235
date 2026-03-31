1fc70a35c48afa72842618eadecbaa10a8c6c69e
xyerakovalskia <xyera123987@gmail.com>
2026-03-31 10:55:20 +0700

    mergesort_10

--- mergesort.cpp
+++ mergesort.cpp
@@ -89,3 +89,20 @@ void mergesort (int low, int high)
         arr[x] = B[x];
     }
 }
+
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    mergesort(0, n - 1);
+    output();
+}
